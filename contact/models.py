1c448dcef918eef0e8649916833fc21c1fe252fb
Ahmed Yasser Asaker <Ahmed.Yasser@GACEGY.LOCAL>
2021-01-24 21:26:51 +0200

    ADD POSTGRESS DB

--- contact/models.py
+++ contact/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    Full_Name=models.CharField(max_length=600)
-    Job_Title=models.CharField(max_length=300)
-    Email= models.EmailField(max_length=254)
-    EXT=models.CharField(max_length=15)
-    Dir_Phone=models.CharField(max_length=15)
-    Mobile=models.CharField(max_length=25)
-    Branch=models.CharField(max_length=300)
+    Full_Name=models.CharField(max_length=600,null=True)
+    Job_Title=models.CharField(max_length=300,null=True)
+    Email= models.EmailField(max_length=254,null=True)
+    EXT=models.CharField(max_length=15,null=True)
+    Dir_Phone=models.CharField(max_length=15,null=True)
+    Mobile=models.CharField(max_length=25,null=True)
+    Branch=models.CharField(max_length=300,null=True)
 
     def __str__(self):
         return self.Full_Name
